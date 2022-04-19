e1cde180feae97286d2e4ec0897738d5b10a52fd
Lisa Huang <LisaHuangAde19@gmail.com>
2022-04-19 11:08:39 +0930

    es5:dom function inherit this

--- closure.js
+++ closure.js
@@ -1,16 +1,33 @@
-function Person(name, age) {
-  var a = 0
-  this.name = name
-  this.age = age
-  function sss() {
-    a++
-    document.write(a)
+function test() {
+  var arr = []
+  for (var i = 0; i < 10; i++) {
+    arr[i] = function () {
+      console.log(i + '')
+    }
   }
-  this.say = sss
+  return arr
 }
+var myArr = test()
 
-var oP = new Person()
-oP.say()
-oP.say()
-var oP1 = new Person()
-oP.say() 
+for (var j = 0; j < 10; j++) {
+  myArr[j]()
+} //=> 10 10 10 ... 10
+
+//--------------------------------------
+function testImprove() {
+  var arr = []
+  for (var i = 0; i < 10; i++) {
+    (function (j) {
+      arr[j] = function () {
+        console.log(j + '')
+      }
+    }(i))
+
+  }
+  return arr
+}
+var myArr1 = testImprove()
+
+for (var j = 0; j < 10; j++) {
+  myArr1[j]()
+} //=> 0 1 3 ... 9
